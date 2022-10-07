143e7d381261089392520c335c69f9d3f2f79a30
Glory Agatevure <agatevureglory@gmail.com>
2022-10-06 18:23:26 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const ALGOD_SERVER = 'https://testnet-algorand.api.purestake.io/ps2'
 const INDEXER_SERVER = 'https://testnet-algorand.api.purestake.io/idx2'
-const TOKEN = { 'X-API-Key': 'fetqTyZ8r82MSX9YT2pLq53iRMZwVibQx3TtrZ2h' }
+const TOKEN = { 'X-API-Key': '' }
 const PORT = '443';
 
 
